93b764e8e37878e57f1be169755a6fd8b094e21e
Jessica Goulding <jgoulding2@gmail.com>
2016-12-05 09:37:31 -0700

    added ex machina

--- mix.exs
+++ mix.exs
@@ -19,7 +19,9 @@ defmodule Retrodoc.Mixfile do
   def application do
     [mod: {Retrodoc, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex,
+                    # above are boilerplate dependencies
+                    :ex_machina]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +39,9 @@ defmodule Retrodoc.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     # above are boilerplate dependencies
+     {:ex_machina, "~> 1.0", only: :test}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,3 +2,5 @@ ExUnit.start
 
 Ecto.Adapters.SQL.Sandbox.mode(Retrodoc.Repo, :manual)
 
+{:ok, _} = Application.ensure_all_started(:ex_machina)
+
